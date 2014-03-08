398ab2e20461b563349cb1293debb7af8eb10a7f
Steve Jackson <steve.jackson@leandogsoftware.com>
2014-03-08 17:45:37 -0500

    Upgrade default to play-2.2.2

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['play2']['version'] = "2.1.3"
+default['play2']['version'] = "2.2.2"
 default['play2']['url'] = "http://downloads.typesafe.com/play/"
 default['play2']['checksum'] = "15a7fca3e955422017acc299827da72b"
 default['play2']['home'] = '/usr/local/play'
